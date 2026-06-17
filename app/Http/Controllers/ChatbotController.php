0b84c56a65717d1f97821c0b072b1681f5bea3ac
jhamile17 <https://github.com/jhamile17/proyectocoolify.git>
2026-06-17 15:13:47 -0500

    controlador chatbot modificado

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -7,29 +7,31 @@ use Illuminate\Support\Facades\Http;
 
 class ChatbotController extends Controller
 {
-    public function send(Request $request)
-    {
-        $message = $request->message;
-        $apiKey = env('GEMINI_API_KEY');
+   public function send(Request $request)
+{
+    $message = $request->message;
+    $apiKey = env('GEMINI_API_KEY');
 
-        $response = Http::post(
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={$apiKey}",
-            [
-                "contents" => [
-                    [
-                        "parts" => [
-                            [
-                                "text" => $message
-                            ]
+    $response = Http::post(
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={$apiKey}",
+        [
+            "contents" => [
+                [
+                    "parts" => [
+                        [
+                            "text" => $message
                         ]
                     ]
                 ]
             ]
-        );
+        ]
+    );
 
-        return response()->json([
-            'status' => $response->status(),
-            'data' => $response->json()
-        ]);
-    }
+    $data = $response->json();
+
+    return response()->json([
+        'reply' => $data['candidates'][0]['content']['parts'][0]['text']
+            ?? 'No pude generar una respuesta.'
+    ]);
+}
 }
