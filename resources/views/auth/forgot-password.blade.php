87cf207d03144f3dd5c913acdb854b793c85927d
Sayuri <i2413259@continental.edu.pe>
2026-06-23 00:47:50 -0500

    prueba

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,9 +1,9 @@
-<h1>TEST</h1>
+<h2>TEST FORM</h2>
 
 <form method="POST" action="{{ route('password.email') }}">
     @csrf
 
-    <input type="email" name="email" value="test@test.com">
+    <input type="email" name="email" value="test@test.com" required>
 
     <button type="submit">ENVIAR</button>
 </form>
